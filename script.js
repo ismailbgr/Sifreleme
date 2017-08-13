3f940273227f6934b9b0577abf01a8e4f55c0115
ismailbgr <ozismailbugra@gmail.com>
2017-08-13 14:56:06 +0300

     

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ var a = encodeURI(yazi)
 var b = window.btoa(a)
 var c = window.btoa(b)
 var d = window.btoa(c)
+var e = window.btoa(d)
 console.log(a)
 console.log(b)
 console.log(c)
 console.log(d)
-prompt("Sonuç",d)
+console.log(e)
+prompt("Sonuç",e)
 }
 }
 
@@ -21,16 +23,18 @@ var yazi = prompt("Şifresini Çözmek İstediğiniz Yazıyı Yazınız")
 var a = window.atob(yazi)
 var b = window.atob(a)
 var c = window.atob(b)
-var d = decodeURI(c)
+var d = window.atob(c)
+var e = decodeURI(d)
 console.log(yazi)
 console.log(a)
 console.log(b)
 console.log(c)
 console.log(d)
+console.log(e)
 if(c=="null"){
 console.log("null bulundu")
 alert("Hata 01 Gönderilen Yazının İçeriği Boş")
 }else{
-prompt("Sonuç",d)
+prompt("Sonuç",e)
 }
 }
